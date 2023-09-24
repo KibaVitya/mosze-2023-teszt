7fa85be7351bdc236fcb4d0dd396fc87f324b78a
Viktor Kovalter <kviktor@ibookr.com>
2023-09-24 22:59:20 +0200

    Main.cpp hibáinak kommentelése

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; // NELEMENTS helyett N_ELEMENTS-szet kell használni
+    std::cout << '1-100 ertekek duplazasa' // Pontosvessző, nem jó idézőjelek.
+    for (int i = 0;) // Ez nem for ciklus, nem növel semmit, nem történik semmi.
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) // A ciklus feltétel, ciklusváltozó növelése hibás
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //Pontosvessző, nem ír ki semmit csak, hogy "Ertek:"
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++) // Pontosvessző kell vessző helyett
     {
-        atlag += b[i]
+        atlag += b[i] //Végére pontosvessző
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
