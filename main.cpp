b6586354aa72c5c222fa3821de95d29a27adfddf
Viktor Kovalter <kviktor@ibookr.com>
2023-09-24 23:03:06 +0200

    Main.cpp hibáinak javítása, működőképes verzióra átírása

--- main.cpp
+++ main.cpp
@@ -2,25 +2,28 @@
 
 constexpr int N_ELEMENTS = 100;
 
-int main()
-{
-    int *b = new int[NELEMENTS]; // NELEMENTS helyett N_ELEMENTS-szet kell használni
-    std::cout << '1-100 ertekek duplazasa' // Pontosvessző, nem jó idézőjelek.
-    for (int i = 0;) // Ez nem for ciklus, nem növel semmit, nem történik semmi.
-    {
-        b[i] = i * 2;
+int main() {
+    int *arr = new int[N_ELEMENTS];
+
+    for (int i = 0; i < N_ELEMENTS; i++) {
+        arr[i] = i * 2;
     }
-    for (int i = 0; i; i++) // A ciklus feltétel, ciklusváltozó növelése hibás
-    {
-        std::cout << "Ertek:" //Pontosvessző, nem ír ki semmit csak, hogy "Ertek:"
-    }    
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++) // Pontosvessző kell vessző helyett
-    {
-        atlag += b[i] //Végére pontosvessző
+
+    std::cout << "Az 1-100 szamok duplazasa:" << std::endl;
+    for (int i = 0; i < N_ELEMENTS; i++) {
+        std::cout << "Ertek: " << arr[i] << std::endl;
+    }
+
+    int atlag = 0;
+    for (int i = 0; i < N_ELEMENTS; i++) {
+        atlag += arr[i];
     }
     atlag /= N_ELEMENTS;
+
+    std::cout << "Atlag szamitasa: " << std::endl;
     std::cout << "Atlag: " << atlag << std::endl;
+
+    delete[] arr;
+
     return 0;
 }
